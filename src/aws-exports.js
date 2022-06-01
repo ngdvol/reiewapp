c2a9bc7ba8259286d4494ef76673b4808febd850
An Loc <anloc@yahoo.com>
2022-06-01 23:54:02 +0800

    new amplify init

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,10 +3,10 @@
 
 const awsmobile = {
     "aws_project_region": "ap-southeast-1",
-    "aws_appsync_graphqlEndpoint": "https://mxkn6xkngngghambkoige2wzli.appsync-api.ap-southeast-1.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://5mvmiflmvbclvhlytfpzxgckgi.appsync-api.ap-southeast-1.amazonaws.com/graphql",
     "aws_appsync_region": "ap-southeast-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-hww3mt3ejje4pbtt7hi7yi7cva"
+    "aws_appsync_apiKey": "da2-pdfse6xnkzdjvl66bkvwusyjkm"
 };
 
 
